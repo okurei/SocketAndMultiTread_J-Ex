5c428cb1f67177688c855efe57f5adcc5e6967bd
okurei <167979336+okurei@users.noreply.github.com>
2024-10-17 15:58:21 +0200

    First try at a multiThread and socket. Not real good but an honest one.

--- src/main/java/com/firstTryMaven/SocketServer.java
+++ src/main/java/com/firstTryMaven/SocketServer.java
@@ -3,19 +3,23 @@ package com.firstTryMaven;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SocketServer {
     public static void main(String[] args){
         int port = 1234;
+        Scanner scan = new Scanner(System.in);
         ExecutorService executor = Executors.newCachedThreadPool();
         try (ServerSocket serverSocket = new ServerSocket(port)){
             System.out.println("Server open on port " + port);
-            while (true){
+            String exit = "run";
+            while (!(exit.equals("exit"))){
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected "+ clientSocket.getRemoteSocketAddress());
                 executor.execute(new ClientHandler(clientSocket));
+                exit = scan.nextLine();
             }
 
         }catch (IOException e){
