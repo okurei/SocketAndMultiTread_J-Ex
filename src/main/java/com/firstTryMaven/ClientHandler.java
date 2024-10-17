e97e9da6f2f4923c93fc09d083dcf00ce0f3cee0
okurei <167979336+okurei@users.noreply.github.com>
2024-10-17 15:58:50 +0200

    First try at a multiThread and socket. Not real good but an honest one.

--- src/main/java/com/firstTryMaven/ClientHandler.java
+++ src/main/java/com/firstTryMaven/ClientHandler.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 
 public class ClientHandler implements Runnable{
 
-    private Socket clientSocket;
+    private final Socket clientSocket;
 
     public ClientHandler(Socket socket){
         this.clientSocket = socket;
